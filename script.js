e2c58e69a34760fb7b9f897cd5be05b2fdfabdce
Mxwsic <danielapacible2@gmail.com>
2023-01-11 21:08:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 // First make a new branch. Push into the branch, someone needs to approve it.
 // After it gets approved, it will get merged into main. 
 
+<<<<<<< Updated upstream
 var wheel = document.querySelector('.container');
+=======
+var wheel = document.querySelector('.wheel');
+>>>>>>> Stashed changes
 var spinBtn = document.querySelector('.buttonparent');
 var rng = Math.ceil(Math.random() * 3600);
 
@@ -11,5 +15,21 @@ spinBtn.onclick = function() {
     rng += Math.ceil(Math.random() * 3600); 
 }
 
+<<<<<<< Updated upstream
 
 
+=======
+// Recipe finder
+//var recipeSearch = document.querySelector("");
+
+//recipeSearch.addEventListener("click", (){
+  //  recipeSearch();
+//})
+
+//function recipeSearch(){
+  //  var appID = "152ce9d9";
+    //var appKey = "0c7f1fb6a280c124d6bfdd378fe124af";
+   // var recipeResponse = await fetch(`https://api.edamam.com/api/recipes/v2?app_id={appID}&app_key={appKey}&q=chicken`);
+    //console.log(recipeResponse);
+//}
+>>>>>>> Stashed changes

e028aa86bdf8312aa6d1dbe557127ef132660f34
Reptartheman <117610334+Reptartheman@users.noreply.github.com>
2023-01-11 21:14:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // First make a new branch. Push into the branch, someone needs to approve it.
 // After it gets approved, it will get merged into main. 
 
+<<<<<<< Updated upstream
+=======
+var wheel = document.querySelector('.wheel');
+var spinBtn = document.querySelector('.buttonparent');
+var rng = Math.ceil(Math.random() * 3600);
+
+// Giving the button the wheel spin function.
+spinBtn.onclick = function() {
+    wheel.style.transform = "rotate(" + rng + "deg)";
+    rng += Math.ceil(Math.random() * 3600); 
+}
+>>>>>>> Stashed changes
 
 
 
